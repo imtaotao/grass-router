756a8ecbe9e0a30281474602b36f6b04eb4577fc
imtaotao <dwchentao@163.com>
2019-08-03 19:38:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## grass-router
+这是 grass 的配套路由库
---
+++ build.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const rollup = require('rollup')
+const rm = require('rimraf').sync
+const babel = require('rollup-plugin-babel')
+const cmd = require('rollup-plugin-commonjs')
+const cleanup = require('rollup-plugin-cleanup')
+const resolve = require('rollup-plugin-node-resolve')
+const typescript = require('rollup-plugin-typescript2')
+
+const esm = {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/grass-router.esm.js',
+    format: 'es',
+  }
+}
+
+const umd = {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/grass-router.min.js',
+    format: 'umd',
+    name: 'GrassRouter',
+  }
+}
+
+const cjs = {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/grass-router.common.js',
+    format: 'cjs',
+  }
+}
+
+async function build (cfg, sourcemap = false) {
+  cfg.output.sourcemap = sourcemap
+
+  const bundle = await rollup.rollup({
+    input: cfg.input,
+    plugins: [
+      cleanup(),
+      resolve(),
+      babel({
+        babelrc: true,
+        exclude: 'node_modules/**',
+      }),
+      typescript({
+        tsconfig: 'tsconfig.json',
+      }),
+      cmd(),
+    ]
+  })
+  await bundle.generate(cfg.output)
+  await bundle.write(cfg.output)
+}
+
+// delete old build files
+rm('./dist')
+build(esm)
+build(cjs)
+build(umd)
+
+// watch, use in dev and test
+if (process.argv.includes('-w')) {
+  let i = 0
+  fs.watch('./src', () => {
+    build(esm, true)
+    build(cjs, true)
+    build(umd, true)
+    console.clear()
+    console.log('Rebuild times: ' + ++i)
+  })
+}
